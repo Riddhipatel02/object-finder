cf5ac7163c5da3db7b21167b8491882ef662c242
Riddhipatel02 <74219262+Riddhipatel02@users.noreply.github.com>
2022-01-31 19:59:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+objects=[];
+status="";
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
